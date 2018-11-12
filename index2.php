676c077da506a9531aa323811d32c8a0f459a94a
tetsuwrx <tetsuwrx@gmail.com>
2018-11-13 01:23:17 +0900

    Update index2.php

---
+++ index2.php
@@ -0,0 +1,13 @@
+<html>
+  <?php include('index_function.php'); ?>
+  <head>
+    <title>Pool Records</title>
+    <meta name="viewport" content="width=device-width" >
+    <link rel="stylesheet" type="text/css" href="index.css">
+    <script type="text/javascript" src="index.js"></script>
+    <meta http-equiv="Content-Type" content="text/html;charset=utf-8"/>
+  </head>
+  <body>
+      <div><a href="<?php basename(__FILE__);?>">Goto top</a></div>
+  </body>
+</html>
